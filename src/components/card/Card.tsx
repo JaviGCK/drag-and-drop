3095753398520690e4e76733a7640e8c572f5de4
Javier García <javigcpgr@gmail.com>
2024-01-03 12:56:40 +0100

    feat(Card.tsx, card.css, card.data.ts):everything working withouth final styles

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,91 @@
+import './card.css';
+import { useState } from 'react';
+import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
+import { Character, character } from './card.data';
+
+type Columns = {
+    [key: string]: Character[];
+};
+
+const maxCharactersPerColumn = 6;
+const initialCharactersPerColumn = 4;
+
+export const Card = () => {
+    const initialColumns: Columns = {
+        column1: character.slice(0, initialCharactersPerColumn),
+        column2: character.slice(initialCharactersPerColumn, initialCharactersPerColumn * 2),
+        column3: character.slice(initialCharactersPerColumn * 2, initialCharactersPerColumn * 3),
+        column4: character.slice(initialCharactersPerColumn * 3, initialCharactersPerColumn * 4),
+        column5: character.slice(initialCharactersPerColumn * 4, initialCharactersPerColumn * 5),
+    };
+
+    const [columns, updateColumns] = useState<Columns>(initialColumns);
+
+    const handleOnDragEnd = (result: DropResult) => {
+        if (!result.destination) {
+            return;
+        }
+
+        const sourceColumn = columns[result.source.droppableId];
+        const destinationColumn = columns[result.destination.droppableId];
+
+        if (destinationColumn.length >= maxCharactersPerColumn) {
+            return;
+        }
+
+        const updatedColumns: Columns = { ...columns };
+
+        const [draggedItem] = sourceColumn.splice(result.source.index, 1);
+        destinationColumn.splice(result.destination.index, 0, draggedItem);
+
+        updateColumns(updatedColumns);
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Demon Slayer</h1>
+            </header>
+            <section className="App-section">
+                <DragDropContext onDragEnd={handleOnDragEnd}>
+                    <div className="columns-container">
+                        {Object.entries(columns).map(([columnId, column]) => (
+                            <Droppable key={columnId} droppableId={columnId} type="CHARACTERS">
+                                {(provided) => (
+                                    <ul
+                                        className="characters"
+                                        {...provided.droppableProps}
+                                        ref={provided.innerRef}
+                                    >
+                                        {column.map(({ id, name, thumb }, index) => (
+                                            <Draggable
+                                                key={id}
+                                                draggableId={`character-${id}`}
+                                                index={index}
+                                            >
+                                                {(provided) => (
+                                                    <li
+                                                        key={`character-${id}`}
+                                                        ref={provided.innerRef}
+                                                        {...provided.draggableProps}
+                                                        {...provided.dragHandleProps}
+                                                    >
+                                                        <div className="characters-thumb">
+                                                            <img src={thumb} alt={`${name} Thumb`} />
+                                                        </div>
+                                                        <p>{name}</p>
+                                                    </li>
+                                                )}
+                                            </Draggable>
+                                        ))}
+                                        {provided.placeholder}
+                                    </ul>
+                                )}
+                            </Droppable>
+                        ))}
+                    </div>
+                </DragDropContext>
+            </section>
+        </div>
+    );
+};
---
+++ src/components/card/card.data.ts
@@ -0,0 +1,113 @@
+export interface Character {
+    id: string;
+    name: string;
+    thumb: string;
+}
+
+export const character: Character[] = [
+    {
+        id: 'tanjiro',
+        name: 'Tanjiro',
+        thumb: '/public/images/tmb-tanjiro.png'
+    },
+    {
+        id: 'nezuko',
+        name: 'Nezuko',
+        thumb: '/public/images/tmb-nezuko.png'
+    },
+    {
+        id: 'nezuko_demon',
+        name: 'Nezuko Demon',
+        thumb: '/public/images/tmb-nezuko_advanceDemon.png'
+    },
+    {
+        id: 'zenitsu',
+        name: 'Zenitsu',
+        thumb: '/public/images/tmb-zenitsu.png'
+    },
+    {
+        id: 'inosuke',
+        name: 'Inosuke',
+        thumb: '/public/images/tmb-inosuke.png'
+    },
+    {
+        id: 'akaza',
+        name: 'Akaza',
+        thumb: '/public/images/tmb-akaza.png'
+    },
+    {
+        id: 'daki',
+        name: 'Daki',
+        thumb: '/public/images/tmb-daki.jpg'
+    },
+    {
+        id: 'enmu',
+        name: 'Enmu',
+        thumb: '/public/images/tmb-enmu.png'
+    },
+    {
+        id: 'giyu',
+        name: 'Giyu',
+        thumb: '/public/images/tmb-giyu.png'
+    },
+    {
+        id: 'gyutaro',
+        name: 'Gyutaro',
+        thumb: '/public/images/tmb-gyutaro.jpg'
+    },
+    {
+        id: 'makomo',
+        name: 'Makomo',
+        thumb: '/public/images/tmb-makomo.png'
+    },
+    {
+        id: 'murata',
+        name: 'Murata',
+        thumb: '/public/images/tmb-murata.png'
+    },
+    {
+        id: 'rengoku',
+        name: 'Rengoku',
+        thumb: '/public/images/tmb-rengoku.png'
+    },
+    {
+        id: 'rui',
+        name: 'Rui',
+        thumb: '/public/images/tmb-rui.png'
+    },
+    {
+        id: 'sabito',
+        name: 'Sabito',
+        thumb: '/public/images/tmb-sabito.png'
+    },
+    {
+        id: 'shinobu',
+        name: 'Shinobu',
+        thumb: '/public/images/tmb-shinobu.png'
+    },
+    {
+        id: 'susamaru',
+        name: 'Susamaru',
+        thumb: '/public/images/tmb-susamaru.png'
+    },
+    {
+        id: 'tengen',
+        name: 'Tengen',
+        thumb: '/public/images/tmb-tengen.png'
+    },
+    {
+        id: 'urokodaki',
+        name: 'Urokodaki',
+        thumb: '/public/images/tmb-urokodaki.png'
+    },
+    {
+        id: 'yanaba',
+        name: 'Yanaba',
+        thumb: '/public/images/tmb-yahaba.png'
+    },
+    {
+        id: 'yushirotamayo',
+        name: 'Yushiro & Tamayo',
+        thumb: '/public/images/tmb-yushirotamayo.png'
+    }
+]
